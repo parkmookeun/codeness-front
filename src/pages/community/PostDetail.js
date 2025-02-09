bc7c93c70202fdfc20da9fd2500ef921857ee2e8
parkmookeun <sohappynow12@naver.com>
2025-02-09 14:40:17 +0000

    ♻️ Refactor : 게시글 및 댓글 기본 이미지 변경
    ♻️ Refactor : 게시글 및 댓글 기본 이미지 변경

--- src/pages/community/PostDetail.js
+++ src/pages/community/PostDetail.js
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from "react";
 import {useParams, useHistory} from "react-router-dom";
 import api from "../../api/axios";
+import { DEFAULT_PROFILE_IMAGE } from '../../assets/constants';
+
 const PostDetailPage = () => {
   const {postId} = useParams();
   const history = useHistory();
@@ -202,7 +204,7 @@ const PostDetailPage = () => {
                 <div className="writer-info">
                   <img
                       src={post.writerProfileUrl
-                          || "https://images.unsplash.com/photo-1734613876170-079f67aa0d15?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D"}
+                          || DEFAULT_PROFILE_IMAGE}
                       alt="프로필"
                       style={{ width: 30, height: 30, borderRadius: '50%', flexShrink: 0 }}
                       className="profile-img"
@@ -236,7 +238,7 @@ const PostDetailPage = () => {
                     <div className="comment-header">
                       <img
                           src={comment.writerProfileUrl
-                              || "https://images.unsplash.com/photo-1734613876170-079f67aa0d15?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxNnx8fGVufDB8fHx8fA%3D%3D"}
+                              || DEFAULT_PROFILE_IMAGE}
                           alt="프로필"
                           style={{ width: 30, height: 30, borderRadius: '50%', flexShrink: 0 }}
                           className="profile-img"
